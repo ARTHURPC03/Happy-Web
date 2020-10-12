94ffadb59043e49a79162fe5e9a807dacdc1b7f8
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-12 06:42:59 -0300

    Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { FiArrowRight } from 'react-icons/fi'
 
 import GlobalStyle from './styles/global'
+import {
+  PageLanding,
+  ContentWrapper,
+  Location,
+  EnterApp,
+} from './styles/pages/landing'
+
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <PageLanding>
+        <ContentWrapper>
+          <img src={logoImg} alt="Happy" />
+
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+
+          <Location>
+            <strong>Serra</strong>
+            <span>Espírito Santo</span>
+          </Location>
+
+          <EnterApp href="">
+            <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+          </EnterApp>
+        </ContentWrapper>
+      </PageLanding>
       <GlobalStyle />
     </>
   )
---
+++ src/styles/pages/landing.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import img from '../../images/landing.svg'
+
+export const PageLanding = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const ContentWrapper = styled.div`
+  position: relative;
+
+  width: 100%;
+  max-width: 1100px;
+
+  height: 100%;
+  max-height: 680px;
+
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+
+  background: url(${img}) no-repeat 80% center;
+
+  main {
+    max-width: 350px;
+  }
+
+  main h1 {
+    font-size: 76px;
+    font-weight: 900;
+    line-height: 70px;
+  }
+
+  main p {
+    margin-top: 40px;
+    font-size: 24px;
+    line-height: 34px;
+  }
+`
+
+export const Location = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+
+  font-size: 24px;
+  line-height: 34px;
+
+  display: flex;
+  flex-direction: column;
+
+  text-align: right;
+
+  strong {
+    font-weight: 800;
+  }
+`
+
+export const EnterApp = styled.a`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  width: 80px;
+  height: 80px;
+  background: #ffd666;
+  border-radius: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: #96feff;
+  }
+`
